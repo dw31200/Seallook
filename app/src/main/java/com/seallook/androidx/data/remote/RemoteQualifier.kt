37cff4ae74eafba4dbc10dabb28e5653cac1f2e5
dw31200 <sdw31200@naver.com>
2023-09-13 06:25:55 +0900

    #5 원탭 로그인 클라이언트 구성

--- app/src/main/java/com/seallook/androidx/data/remote/RemoteQualifier.kt
+++ app/src/main/java/com/seallook/androidx/data/remote/RemoteQualifier.kt
@@ -5,3 +5,7 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class RemoteCoroutine
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WebClientId
