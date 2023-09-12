f1f5dc072f120b7a835e544ac7d004b5ee816524
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-13 06:27:04 +0900

    [ TSK-21 ] #5 원탭 로그인 클라이언트 구성 (#6)
    * #5 commit
    
    * #5 원탭 로그인 클라이언트 구성

--- app/src/main/java/com/seallook/androidx/data/remote/RemoteModule.kt
+++ app/src/main/java/com/seallook/androidx/data/remote/RemoteModule.kt
@@ -1,5 +1,9 @@
 package com.seallook.androidx.data.remote
 
+import android.content.Context
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -8,6 +12,7 @@ import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -29,4 +34,30 @@ object RemoteModule {
     @RemoteCoroutine
     @Provides
     fun provideCoroutineScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
+    @Provides
+    fun provideOneTapClient(
+        @ApplicationContext context: Context,
+    ): SignInClient =
+        Identity.getSignInClient(context)
+
+    @Provides
+    @WebClientId
+    fun provideWebClientId(): String =
+        "131258226069-ifp8m6aob9h2c9uqptc9vqgrvgm7ka1b.apps.googleusercontent.com"
+
+    @Provides
+    fun provideBeginSignInRequest(
+        @WebClientId webClientId: String,
+    ): BeginSignInRequest =
+        BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(webClientId)
+                    .setFilterByAuthorizedAccounts(true)
+                    .build(),
+            )
+            .setAutoSelectEnabled(true)
+            .build()
 }
