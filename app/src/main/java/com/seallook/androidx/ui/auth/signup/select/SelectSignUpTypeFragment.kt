6662fbd07f7ac1d0f9a49741ffd970543ca16136
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-15 01:56:07 +0900

    [TSK-30] 3001 SelectSignUp 화면 구현 (#16)
    * commit
    
    * #16 SelectSignUp 버튼 UI 구현

--- app/src/main/java/com/seallook/androidx/ui/auth/signup/select/SelectSignUpTypeFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/auth/signup/select/SelectSignUpTypeFragment.kt
@@ -1,7 +1,23 @@
 package com.seallook.androidx.ui.auth.signup.select
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentSelectSignUpBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import com.seallook.androidx.ui.base.BaseViewModel
+
 /* TODO
     1.InitView: 내담자, 상담사, 기관 별 가입 버튼 보여주기
     2.Navigation: 각 버튼 클릭시 SignUp으로 이동
  */
-class SelectSignUpTypeFragment
+class SelectSignUpTypeFragment : BaseFragment<FragmentSelectSignUpBinding, BaseViewModel>(
+    FragmentSelectSignUpBinding::inflate,
+) {
+    override val viewModel: BaseViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
