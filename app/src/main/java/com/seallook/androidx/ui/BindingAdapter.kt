4512912f19eed0b6da3150c55601c85415976812
dw31200 <sdw31200@naver.com>
2023-09-15 15:17:46 +0900

    #20 userType에 따른 동적 TextField 구현

--- app/src/main/java/com/seallook/androidx/ui/BindingAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/BindingAdapter.kt
@@ -2,8 +2,14 @@ package com.seallook.androidx.ui
 
 import android.view.View
 import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("bind:visible")
 fun View.setVisible(visible: Boolean) {
     visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("bind:userTypeToHint")
+fun TextInputLayout.setHint(userType: Int) {
+    hint = if (userType == 2) "기관명" else "이름"
 }
