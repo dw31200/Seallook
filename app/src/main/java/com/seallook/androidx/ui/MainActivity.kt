912831816942cf15f26f884bf551581a4c094fe8
dw31200 <sdw31200@naver.com>
2023-09-16 02:33:40 +0900

    #30 bottom navigation bar 기본 세팅

--- app/src/main/java/com/seallook/androidx/ui/MainActivity.kt
+++ app/src/main/java/com/seallook/androidx/ui/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.seallook.androidx.ui
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.seallook.androidx.R
 import com.seallook.androidx.databinding.ActivityMainBinding
 import com.seallook.androidx.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,5 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(
     ActivityMainBinding::inflate,
 ) {
-    override fun onCreateAfterBinding() = Unit
+    override fun onCreateAfterBinding() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navHostFragment.findNavController().also {
+            binding.bottomNavigationView.setupWithNavController(it)
+        }
+    }
 }
