15a46c9faaef925c9ec261d34167ddc8ab2f7ad4
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-15 23:18:17 +0900

    [TSK-47] 4001 앱 시작시 로그인 사용자가 있으면 홈화면 네비게이션 (#29)
    * commit
    
    * #29 Splash 기본 구현 및 currentUser에 따른 동적 navigation
    
    * #29 Splash action popUp 설정

---
+++ app/src/main/java/com/seallook/androidx/ui/splash/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.seallook.androidx.ui.splash
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.seallook.androidx.BR
+import com.seallook.androidx.R
+import com.seallook.androidx.databinding.FragmentSplashBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(
+    FragmentSplashBinding::inflate,
+) {
+    override val viewModel: SplashViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    override fun onViewCreatedAfterBinding() {
+        if (auth.currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/seallook/androidx/ui/splash/SplashViewModel.kt
@@ -0,0 +1,8 @@
+package com.seallook.androidx.ui.splash
+
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor() : BaseViewModel()
