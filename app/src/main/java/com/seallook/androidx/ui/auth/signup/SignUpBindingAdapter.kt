fdd544ff6d17c62a15c069ab8e8be50e2ba77cf6
dw31200 <sdw31200@naver.com>
2023-09-15 20:59:45 +0900

    #24 getProfile 수집 코드 추가

---
+++ app/src/main/java/com/seallook/androidx/ui/auth/signup/SignUpBindingAdapter.kt
@@ -0,0 +1,8 @@
+package com.seallook.androidx.ui.auth.signup
+
+import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputEditText
+
+@BindingAdapter("bind:setTextInProfileInfo")
+fun TextInputEditText.setTextInProfileInfo(profileInfo: String) {
+}
