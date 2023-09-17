a92eb3a3f014362a242d23c1336e2339247fa556
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-17 20:56:34 +0900

    [TSK-53] 7001 채팅 리스트 화면 구현 (#34)
    * commit
    
    * #34 ChatList 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/chat/list/ChatListViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/chat/list/ChatListViewModel.kt
@@ -1,3 +1,8 @@
 package com.seallook.androidx.ui.chat.list
 
-class ChatListViewModel
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatListViewModel @Inject constructor() : BaseViewModel()
