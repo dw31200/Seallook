acbf7da9e0091e371ecf1a1e45b4a11c13415a2c
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-17 20:42:07 +0900

    [TSK-52] 6001 캘린더 화면 구현 (#33)
    * commit
    
    * #33 Calendar 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/calendar/CalendarFragment.kt
@@ -1,5 +1,10 @@
 package com.seallook.androidx.ui.calendar
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentCalendarBinding
+import com.seallook.androidx.ui.base.BaseFragment
+
 /* TODO
     1.GetReserved: 로그인 계정의 예약 현황을 가져오기
     2.InitView: 가져온 일정 달력에 보여주기
@@ -8,4 +13,14 @@ package com.seallook.androidx.ui.calendar
     5.NavigateToReservedDetail: 플로팅뷰에서 특정 일정을 클릭시 해당 일정의 ReservedDetail로 이동
     6.NavigateToSettingSchedule: 기본 화면에서 +버튼 클릭시 SettingSchedule로 이동
  */
-class CalenderFragment
+class CalendarFragment : BaseFragment<FragmentCalendarBinding, CalendarViewModel>(
+    FragmentCalendarBinding::inflate,
+) {
+    override val viewModel: CalendarViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
