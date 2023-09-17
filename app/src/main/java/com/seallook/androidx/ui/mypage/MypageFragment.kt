0ee1c3101366aae091e2a80e66d0cf2252bdb2c5
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-17 23:22:28 +0900

    [TSK-54] 8001 마이페이지 화면 구현 (#35)
    * commit
    
    * #35 Mypage 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/MypageFragment.kt
@@ -1,5 +1,11 @@
 package com.seallook.androidx.ui.mypage
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentMypageBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /* TODO
     1.GetAuthType: 계정 타입 가져오기
     2.ListView: 계정 타입에 따라 화면 보여주기,[내담자(프로필 관리 버튼, 상담신청내역 버튼, 리뷰관리 버튼, 환경설정 버튼),
@@ -9,4 +15,15 @@ package com.seallook.androidx.ui.mypage
         상담사 설정 > UpdateCounselerBasicInfo, 더보기 > ReservedClientList, 매출현황 > SalesList, 정산신청 > CurrentRequestPay,
         정산내역 > RequestedPayList, 예약된 상담 리스트 중 아이템 > ReservedClientDetail
  */
-class MypageFragment
+@AndroidEntryPoint
+class MypageFragment : BaseFragment<FragmentMypageBinding, MypageViewModel>(
+    FragmentMypageBinding::inflate,
+) {
+    override val viewModel: MypageViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
--- app/src/main/java/com/seallook/androidx/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/MypageViewModel.kt
@@ -1,3 +1,8 @@
 package com.seallook.androidx.ui.mypage
 
-class MypageViewModel
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MypageViewModel @Inject constructor() : BaseViewModel()
