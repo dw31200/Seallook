8680ba4346b118fd1b9850c46bd523fa1869ecbd
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-18 03:43:04 +0900

    [TSK-104] 9004 main 각 부속 container navigate 구현 (#39)
    * commit
    
    * #39 각 container navigate 구현

---
+++ app/src/main/java/com/seallook/androidx/ui/diary/setting/DiarySettingViewModel.kt
@@ -0,0 +1,8 @@
+package com.seallook.androidx.ui.diary.setting
+
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DiarySettingViewModel @Inject constructor() : BaseViewModel()
