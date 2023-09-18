0b6b6bead01c43669492d78dd5417463e48ecd61
dw31200 <sdw31200@naver.com>
2023-09-18 23:11:34 +0900

    #40 상담사 검색 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/home/search/counseler/SearchCounselorFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/search/counseler/SearchCounselorFragment.kt
@@ -1,8 +1,25 @@
 package com.seallook.androidx.ui.home.search.counseler
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentSearchCounselorBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /* TODO
     1.GetCounselerList: 검색창의 텍스트가 이름에 들어간 기관 혹은 상담사 리스트 가져오기
     2.ListView: 가져온 상담사 리스트 보여주기. 보여준 뒤에 리스트 중에 기관 클릭시 해당 기관소속 상담사 리스트로 보여주기
     3.Navigation: 상담사 클릭시 해당 상담사 디테일 화면 이동
  */
-class SearchCounselorFragment
+@AndroidEntryPoint
+class SearchCounselorFragment : BaseFragment<FragmentSearchCounselorBinding, SearchCounselorViewModel>(
+    FragmentSearchCounselorBinding::inflate,
+) {
+    override val viewModel: SearchCounselorViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
--- app/src/main/java/com/seallook/androidx/ui/home/search/counseler/SearchCounselorViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/search/counseler/SearchCounselorViewModel.kt
@@ -1,3 +1,8 @@
 package com.seallook.androidx.ui.home.search.counseler
 
-class SearchCounselorViewModel
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchCounselorViewModel @Inject constructor() : BaseViewModel()
