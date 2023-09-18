ecc86052249a0e966c52e1827e33dadb0f16241b
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-19 02:11:31 +0900

    [TSK-58] 12001 예약 신청 화면 구현 (#42)
    * commit
    
    * #42 상담 및 검사 신청서 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/reserve/counseling/form/ReserveFormFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/reserve/counseling/form/ReserveFormFragment.kt
@@ -1,5 +1,11 @@
 package com.seallook.androidx.ui.reserve.counseling.form
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentReserveFormBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /* TODO
     1.GetCounselingForm: 상담 신청서 가져오기
     2.ListView: 가져온 상담 신청서 보여주기
@@ -7,4 +13,15 @@ package com.seallook.androidx.ui.reserve.counseling.form
     4.Navigation: 다음 > ReserveCounselingContract, 뒤로가기 > ReserveCounseling
     5.DeleteForm: 뒤로가기 혹은 앱 종료시 신청서 내용 삭제하기
  */
-class ReserveFormFragment
+@AndroidEntryPoint
+class ReserveFormFragment : BaseFragment<FragmentReserveFormBinding, ReserveFormViewModel>(
+    FragmentReserveFormBinding::inflate,
+) {
+    override val viewModel: ReserveFormViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
