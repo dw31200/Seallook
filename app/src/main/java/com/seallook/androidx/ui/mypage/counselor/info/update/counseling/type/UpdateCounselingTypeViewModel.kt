cc7d3e69951add2354e40a8914a21ca45290f2a4
dw31200 <sdw31200@naver.com>
2023-09-19 04:05:10 +0900

    #46 상담 분류 등록 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/counseling/type/UpdateCounselingTypeViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/counseling/type/UpdateCounselingTypeViewModel.kt
@@ -1,3 +1,8 @@
 package com.seallook.androidx.ui.mypage.counselor.info.update.counseling.type
 
-class UpdateCounselingTypeViewModel
+import com.seallook.androidx.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UpdateCounselingTypeViewModel @Inject constructor() : BaseViewModel()
