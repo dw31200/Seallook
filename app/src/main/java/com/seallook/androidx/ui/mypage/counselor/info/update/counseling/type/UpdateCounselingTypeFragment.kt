d24195c71a68932f94d40554a28d122bd0547117
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-09-19 04:05:22 +0900

    [TSK-78] 29001 상담 분류 등록 화면 구현 (#46)
    * commit
    
    * #46 상담 분류 등록 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/counseling/type/UpdateCounselingTypeFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/counseling/type/UpdateCounselingTypeFragment.kt
@@ -1,7 +1,24 @@
 package com.seallook.androidx.ui.mypage.counselor.info.update.counseling.type
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentUpdateCounselingTypeBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /* TODO
     1.SetView: 추가하기 클릭시 상담인원, 소요시간, 금액, 자장버튼(클릭시 '수정' 변경, '수정'클릭시 역방향 및 수정가능) 노출
     2.Navigation: 다음 > Preview
  */
-class UpdateCounselingTypeFragment
+@AndroidEntryPoint
+class UpdateCounselingTypeFragment : BaseFragment<FragmentUpdateCounselingTypeBinding, UpdateCounselingTypeViewModel>(
+    FragmentUpdateCounselingTypeBinding::inflate,
+) {
+    override val viewModel: UpdateCounselingTypeViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
