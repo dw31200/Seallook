f86319f7177b355cc15e35db121b53788c555ab7
dw31200 <sdw31200@naver.com>
2023-09-19 13:37:32 +0900

    #47 기관 등록 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/office/UpdateOfficeFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/counselor/info/update/office/UpdateOfficeFragment.kt
@@ -1,8 +1,25 @@
 package com.seallook.androidx.ui.mypage.counselor.info.update.office
 
+import androidx.fragment.app.viewModels
+import com.seallook.androidx.BR
+import com.seallook.androidx.databinding.FragmentUpdateOfficeBinding
+import com.seallook.androidx.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
 /* TODO
     1.GetOfficeList: 검색창 돋보기 클릭시 검색창 텍스트가 이름에 들어간 기관 가져오기
     2.ListView: 가져온 리스트 보여주기
     3.Navigation: 기관 아이템/뒤로가기 > BasicInfo
  */
-class UpdateOfficeFragment
+@AndroidEntryPoint
+class UpdateOfficeFragment : BaseFragment<FragmentUpdateOfficeBinding, UpdateOfficeViewModel>(
+    FragmentUpdateOfficeBinding::inflate,
+) {
+    override val viewModel: UpdateOfficeViewModel by viewModels()
+
+    override fun viewModelVariableId(): Int = BR.vm
+
+    override fun onViewCreatedAfterBinding() {
+        Unit
+    }
+}
