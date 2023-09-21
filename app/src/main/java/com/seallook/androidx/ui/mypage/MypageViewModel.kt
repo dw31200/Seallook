a99ab6d9af76b06302e36ba467f557318af65036
dw31200 <sdw31200@naver.com>
2023-09-22 01:53:51 +0900

    #65 usertype에 따른 동적 화면 구현

--- app/src/main/java/com/seallook/androidx/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/mypage/MypageViewModel.kt
@@ -1,8 +1,36 @@
 package com.seallook.androidx.ui.mypage
 
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseUser
+import com.seallook.androidx.domain.model.UserTypeEntity
+import com.seallook.androidx.domain.usecase.GetCurrentUserUseCase
+import com.seallook.androidx.domain.usecase.GetUserTypeUseCase
 import com.seallook.androidx.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class MypageViewModel @Inject constructor() : BaseViewModel()
+class MypageViewModel @Inject constructor(
+    private val getUserTypeUseCase: GetUserTypeUseCase,
+    private val getCurrentUserUseCase: GetCurrentUserUseCase,
+) : BaseViewModel() {
+    val currentUser: StateFlow<FirebaseUser?> = getCurrentUserUseCase()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            null,
+        )
+    val userType: StateFlow<UserTypeEntity> =
+        currentUser
+            .flatMapLatest { currentUser ->
+                getUserTypeUseCase(currentUser)
+            }.stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(),
+                UserTypeEntity(0),
+            )
+}
