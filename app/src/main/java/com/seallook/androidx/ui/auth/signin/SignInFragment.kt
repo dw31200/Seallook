675ef94e8fe529c02148b0c4e4ff77c5a88e9275
dw31200 <sdw31200@naver.com>
2023-09-27 17:48:17 +0900

    #83 signinbase 제거

--- app/src/main/java/com/seallook/androidx/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/seallook/androidx/ui/auth/signin/SignInFragment.kt
@@ -1,16 +1,22 @@
 package com.seallook.androidx.ui.auth.signin
 
+import android.app.Activity
 import android.util.Patterns
+import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.identity.Identity
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.seallook.androidx.BR
 import com.seallook.androidx.R
 import com.seallook.androidx.databinding.FragmentSignInBinding
-import com.seallook.androidx.ui.base.auth.SignInBaseFragment
+import com.seallook.androidx.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 /* TODO
     0.InitView: id,password EditText, 각 로그인 방식 버튼, 회원가입 버튼, 비밀번호 리겟 버튼 보여주기
@@ -23,11 +29,29 @@ import kotlinx.coroutines.launch
  */
 
 @AndroidEntryPoint
-class SignInFragment : SignInBaseFragment<FragmentSignInBinding>(
+class SignInFragment : BaseFragment<FragmentSignInBinding, SignInViewModel>(
     FragmentSignInBinding::inflate,
 ) {
     override val viewModel: SignInViewModel by viewModels()
     override fun viewModelVariableId(): Int = BR.vm
+
+    private val oneTapClient by lazy { Identity.getSignInClient(requireContext()) }
+    private val googleSignInIntentResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+            if (it.resultCode != Activity.RESULT_OK) {
+                Timber.d("${it.resultCode}")
+                cancelSignIn()
+                return@registerForActivityResult
+            }
+            val credential = oneTapClient.getSignInCredentialFromIntent(it.data)
+            val idToken = credential.googleIdToken
+
+            if (idToken != null) {
+                viewModel.signInWithGoogle(idToken)
+            } else {
+                failSignIn()
+            }
+        }
 
     override fun onViewCreatedAfterBinding() {
         with(binding) {
@@ -83,4 +107,48 @@ class SignInFragment : SignInBaseFragment<FragmentSignInBinding>(
             }
         }
     }
+
+    private fun signInWithGoogle() {
+        if (isSigningIn()) return
+
+        startSignIn()
+        viewModel.getBeginSignInResult()
+        viewModel.beginSignInResult.observe(viewLifecycleOwner) {
+            if (it != null) {
+                googleSignInIntentResultLauncher.launch(
+                    IntentSenderRequest.Builder(it.pendingIntent.intentSender).build(),
+                )
+            }
+        }
+    }
+
+    private fun startSignIn() {
+        showProgressDialog("로그인 중... 잠시만 기다려 주세요.")
+    }
+
+    private fun navigation() {
+        viewModel.profile.observe(viewLifecycleOwner) {
+            if (it != null) {
+                findNavController().navigate(R.id.action_signInFragment_to_mainGraphActivity)
+            } else {
+                findNavController().navigate(R.id.action_signInFragment_to_selectSignUpTypeFragment)
+            }
+        }
+    }
+
+    private fun cancelSignIn() {
+        dismissProgressDialog()
+    }
+
+    private fun failSignIn() {
+        dismissProgressDialog()
+
+        Toast.makeText(
+            requireContext(),
+            "오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.",
+            Toast.LENGTH_SHORT,
+        ).show()
+    }
+
+    private fun isSigningIn() = isProgressDialogShown()
 }
