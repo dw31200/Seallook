aecccf2060fcba75fd58e78253d06717dada7bf1
dw31200 <sdw31200@naver.com>
2023-09-27 22:51:10 +0900

    #85 usecase livedata 변경 준비

--- app/src/main/java/com/seallook/androidx/domain/usecase/SetUserTypeUseCase.kt
+++ app/src/main/java/com/seallook/androidx/domain/usecase/SetUserTypeUseCase.kt
@@ -1,5 +1,7 @@
 package com.seallook.androidx.domain.usecase
 
+import com.google.firebase.auth.FirebaseUser
+import com.seallook.androidx.data.repository.FirebaseFirestoreRepository
 import com.seallook.androidx.data.repository.SetUserTypeRepository
 import com.seallook.androidx.domain.model.UserTypeEntity
 import dagger.Reusable
@@ -8,8 +10,13 @@ import javax.inject.Inject
 @Reusable
 class SetUserTypeUseCase @Inject constructor(
     private val setUserTypeRepository: SetUserTypeRepository,
+    private val firebaseFirestoreRepository: FirebaseFirestoreRepository,
 ) {
     suspend operator fun invoke(user: UserTypeEntity) {
         setUserTypeRepository.setUserType(user.toUserType())
+    }
+
+    suspend fun setUserType(user: FirebaseUser?, userType: UserTypeEntity) {
+        firebaseFirestoreRepository.setUserType(user, userType.toUserType())
     }
 }
