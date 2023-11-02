7dc211ac0c8b02b475fa939c6148eaa3adf52c66
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-11-02 23:32:16 +0900

    [TSK-209] 10005 상담사 아이템 클릭시 상담예약 화면으로 이동 (#129)
    * commit
    
    * #129 상담사 아이템 클릭시 상담예약 화면으로 이동

--- app/src/main/java/com/seallook/androidx/ui/home/search/counseler/adapter/SearchCounselorAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/search/counseler/adapter/SearchCounselorAdapter.kt
@@ -3,11 +3,14 @@ package com.seallook.androidx.ui.home.search.counseler.adapter
 import android.annotation.SuppressLint
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.seallook.androidx.ui.home.search.counseler.SearchCounselorNavigation
 import com.seallook.androidx.ui.model.CounselorInfoUiModel
 
 class SearchCounselorAdapter(
     private val counselorInfoItems: MutableList<CounselorInfoUiModel> = mutableListOf(),
 ) : RecyclerView.Adapter<SearchCounselorHolder>() {
+    var searchCounselorNavigation: SearchCounselorNavigation? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchCounselorHolder {
         return SearchCounselorHolder(parent)
     }
@@ -17,7 +20,7 @@ class SearchCounselorAdapter(
     }
 
     override fun onBindViewHolder(holder: SearchCounselorHolder, position: Int) {
-        holder.bind(counselorInfoItems[position])
+        holder.bind(counselorInfoItems[position], searchCounselorNavigation)
     }
 
     @SuppressLint("NotifyDataSetChanged")
--- app/src/main/java/com/seallook/androidx/ui/reserve/counseling/ReserveCounselingViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/reserve/counseling/ReserveCounselingViewModel.kt
@@ -1,9 +1,14 @@
 package com.seallook.androidx.ui.reserve.counseling
 
+import androidx.lifecycle.SavedStateHandle
 import com.seallook.androidx.base.Effect
 import com.seallook.androidx.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ReserveCounselingViewModel @Inject constructor() : BaseViewModel<Effect>()
+class ReserveCounselingViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : BaseViewModel<Effect>() {
+    var email = savedStateHandle.get<String>("email")
+}
