5afad50d47c99c51631d7763f71df89e280b43c1
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-12-08 00:06:29 +0900

    [TSK-268] 32002 Reservation 서버에서 가져오기 remote, repo, usecase 구현 (#160)
    * commit
    
    * #160 Reservation 서버에서 가져오기 remote, repo, usecase 구현

--- app/src/main/java/com/seallook/androidx/data/repository/counselor/reservation/ReservationRepository.kt
+++ app/src/main/java/com/seallook/androidx/data/repository/counselor/reservation/ReservationRepository.kt
@@ -3,7 +3,9 @@ package com.seallook.androidx.data.repository.counselor.reservation
 import com.seallook.androidx.data.model.Reservation
 
 interface ReservationRepository {
-    suspend fun getList(email: String): List<Reservation>
+    suspend fun getClientList(email: String): List<Reservation>
+
+    suspend fun getCounselorList(email: String): List<Reservation>
 
     suspend fun set(reservation: Reservation)
 }
