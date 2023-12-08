ceea5cb5f378fb6d60849e2b487cbb14af02d05f
dw31200 <74292450+dw31200@users.noreply.github.com>
2023-12-08 14:01:13 +0900

    [TSK-326] 47003 뷰홀더 databinding으로 변경 (#166)
    * commit
    
    * #166 뷰홀더 databinding으로 변경

--- app/src/main/java/com/seallook/androidx/ui/reserved/counseling/list/ReservedCounselingListBindingAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/reserved/counseling/list/ReservedCounselingListBindingAdapter.kt
@@ -1,11 +1,29 @@
 package com.seallook.androidx.ui.reserved.counseling.list
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.seallook.androidx.ui.model.ReservationUiModel
 import com.seallook.androidx.ui.reserved.counseling.list.adapter.ReservedCounselingListAdapter
+import java.text.SimpleDateFormat
+import java.util.Date
 
 @BindingAdapter("bind:setCounselingList")
 fun RecyclerView.setList(list: List<ReservationUiModel>?) {
     (adapter as? ReservedCounselingListAdapter)?.fetchData(list ?: emptyList())
+}
+
+@BindingAdapter("bind:setDate")
+fun TextView.setDate(date: Date) {
+    val formatter = SimpleDateFormat("yyyy.MM.dd HH:mm")
+    text = formatter.format(date)
+}
+
+@BindingAdapter("bind:setConfirm")
+fun TextView.setConfirm(confirm: Boolean) {
+    if (confirm) {
+        text = "예약 확정"
+    } else {
+        text = "예약 미확정"
+    }
 }
