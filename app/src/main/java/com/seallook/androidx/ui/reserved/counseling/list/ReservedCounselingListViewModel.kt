d62ee5a86770ea05b33986a61216b30604ba85a5
dw31200 <sdw31200@naver.com>
2023-12-17 20:19:23 +0900

    #172 Reservation Flow 연결

--- app/src/main/java/com/seallook/androidx/ui/reserved/counseling/list/ReservedCounselingListViewModel.kt
+++ app/src/main/java/com/seallook/androidx/ui/reserved/counseling/list/ReservedCounselingListViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.asLiveData
 import com.seallook.androidx.base.Effect
-import com.seallook.androidx.domain.usecase.reserved.GetReservedCounselingListSnapshotUseCase
+import com.seallook.androidx.domain.usecase.reserved.GetReservedCounselingListUseCase
 import com.seallook.androidx.ui.base.BaseViewModel
 import com.seallook.androidx.ui.model.ReservationUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,13 +14,13 @@ import javax.inject.Inject
 @HiltViewModel
 class ReservedCounselingListViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getReservedCounselingListSnapshotUseCase: GetReservedCounselingListSnapshotUseCase,
+    private val getReservedCounselingListUseCase: GetReservedCounselingListUseCase,
 ) : BaseViewModel<Effect>() {
     var email = savedStateHandle.get<String>("email")
 
     val reservedCounselingList: LiveData<List<ReservationUiModel>>? =
         email?.let {
-            getReservedCounselingListSnapshotUseCase(it).map {
+            getReservedCounselingListUseCase(it).map {
                 it.map {
                     ReservationUiModel(it)
                 }
