1c0a89e39b07e488509929f4bc5d7e8b679efe8b
dw31200 <sdw31200@naver.com>
2024-01-05 16:23:23 +0900

    #180 아이템 클릭시 링크 페이지 열기

--- app/src/main/java/com/seallook/androidx/ui/home/adapter/OfficeViewHolder.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/adapter/OfficeViewHolder.kt
@@ -4,14 +4,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.seallook.androidx.databinding.OfficeListItemBinding
+import com.seallook.androidx.ui.home.HomeShowWebSite
 import com.seallook.androidx.ui.model.KakaoSearchUiModel
 
 class OfficeViewHolder(
     private val binding: OfficeListItemBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(officeItem: KakaoSearchUiModel?) {
+    fun bind(officeItem: KakaoSearchUiModel?, homeShowWebSite: HomeShowWebSite?) {
         with(binding) {
             data = officeItem
+            officeThumbnail.setOnClickListener {
+                if (officeItem != null) {
+                    homeShowWebSite?.show(officeItem.place_url)
+                }
+            }
         }
     }
 
