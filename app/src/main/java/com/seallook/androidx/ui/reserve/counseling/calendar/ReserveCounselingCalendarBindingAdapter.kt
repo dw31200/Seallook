b7c19f1945e4de2d81b14222f78c6a8c40e5bc4c
dw31200 <sdw31200@naver.com>
2024-01-25 01:44:19 +0900

    스케쥴 등록 날짜 뱃지 보여주기

--- app/src/main/java/com/seallook/androidx/ui/reserve/counseling/calendar/ReserveCounselingCalendarBindingAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/reserve/counseling/calendar/ReserveCounselingCalendarBindingAdapter.kt
@@ -2,6 +2,7 @@ package com.seallook.androidx.ui.reserve.counseling.calendar
 
 import androidx.databinding.BindingAdapter
 import com.kizitonwose.calendar.view.WeekCalendarView
+import com.seallook.androidx.ui.model.CounselingScheduleUiModel
 import java.time.LocalDate
 
 @BindingAdapter("bind:reserveSelectedDate")
@@ -13,4 +14,10 @@ fun WeekCalendarView.setSelectedDate(selectedDate: LocalDate?) {
 @BindingAdapter("bind:setOnClickDay")
 fun WeekCalendarView.setOnClickDay(reserveCounselingSelectDate: ReserveCounselingSelectDate) {
     (dayBinder as? ReserveCounselingCalendarBinder)?.reserveCounselingSelectDate = reserveCounselingSelectDate
+}
+
+@BindingAdapter("bind:scheduleItems")
+fun WeekCalendarView.setScheduleItems(scheduleItems: List<CounselingScheduleUiModel>?) {
+    (dayBinder as? ReserveCounselingCalendarBinder)?.setScheduleItems(scheduleItems ?: emptyList())
+    notifyCalendarChanged()
 }
