b10b23c6c71abefdaa5daa179d65d50fc636dfb6
dw31200 <sdw31200@naver.com>
2024-01-26 13:42:42 +0900

    화면 재구성

--- app/src/main/java/com/seallook/androidx/ui/home/HomeBindingAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/HomeBindingAdapter.kt
@@ -3,7 +3,6 @@ package com.seallook.androidx.ui.home
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
 import com.seallook.androidx.ui.home.adapter.HomeAdapter
 import com.seallook.androidx.ui.home.adapter.OfficeListAdapter
 import com.seallook.androidx.ui.model.CounselorInfoUiModel
@@ -20,8 +19,8 @@ fun RecyclerView.setOfficeList(list: List<KakaoSearchUiModel>?) {
 }
 
 @BindingAdapter("bind:setDistance")
-fun TextView.setDistance(distance: String) {
-    text = "${distance}m"
+fun TextView.setDistance(distance: String?) {
+    text = if (distance != null) "${distance}m" else ""
 }
 
 @BindingAdapter("bind:onOfficeItemClickListener")
@@ -32,22 +31,4 @@ fun RecyclerView.setOnOfficeItemClickListener(homeShowWebSite: HomeShowWebSite) 
 @BindingAdapter("bind:onItemClickListener")
 fun RecyclerView.setCounselorItemClickListener(homeNavigation: HomeNavigation) {
     (adapter as? HomeAdapter)?.homeNavigation = homeNavigation
-}
-
-@BindingAdapter("bind:onReservedClientListButtonClickListener", "bind:userEmail", requireAll = true)
-fun MaterialButton.setOnReservedClientListButtonClickListener(homeNavigation: HomeNavigation, userEmail: String?) {
-    setOnClickListener {
-        if (userEmail != null) {
-            homeNavigation.navigateToReservedCounseling(userEmail)
-        }
-    }
-}
-
-@BindingAdapter("bind:onReservedCounselingListButtonClickListener", "bind:userEmail", requireAll = true)
-fun MaterialButton.setOnReservedCounselingListButtonClickListener(homeNavigation: HomeNavigation, userEmail: String?) {
-    setOnClickListener {
-        if (userEmail != null) {
-            homeNavigation.navigateToReservedCounseling(userEmail)
-        }
-    }
 }
