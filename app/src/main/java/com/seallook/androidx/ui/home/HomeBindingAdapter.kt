0169ce46121dd17bad300416d9666b1966eb99ff
dw31200 <sdw31200@naver.com>
2024-01-28 00:28:09 +0900

    상담센터 이미지 가져와서 보여주기

--- app/src/main/java/com/seallook/androidx/ui/home/HomeBindingAdapter.kt
+++ app/src/main/java/com/seallook/androidx/ui/home/HomeBindingAdapter.kt
@@ -1,12 +1,23 @@
 package com.seallook.androidx.ui.home
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.seallook.androidx.ui.home.adapter.HomeAdapter
 import com.seallook.androidx.ui.home.adapter.OfficeListAdapter
 import com.seallook.androidx.ui.model.CounselorInfoUiModel
 import com.seallook.androidx.ui.model.KakaoSearchUiModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import timber.log.Timber
 
 @BindingAdapter("bind:setList")
 fun RecyclerView.setList(list: List<CounselorInfoUiModel>?) {
@@ -31,4 +42,31 @@ fun RecyclerView.setOnOfficeItemClickListener(homeShowWebSite: HomeShowWebSite) 
 @BindingAdapter("bind:onItemClickListener")
 fun RecyclerView.setCounselorItemClickListener(homeNavigation: HomeNavigation) {
     (adapter as? HomeAdapter)?.homeNavigation = homeNavigation
+}
+
+@BindingAdapter("bind:officeUrl")
+fun ImageView.setOfficeUrl(url: String) {
+    this.findViewTreeLifecycleOwner()?.lifecycleScope?.launch {
+        var imageUrl: String? = null
+
+        withContext(Dispatchers.IO) {
+            val doc: Document = Jsoup.connect(url).get()
+
+            val ogImageElement: Element? = doc.select("meta[property=og:image]").first()
+
+            if (ogImageElement != null) {
+                imageUrl = ogImageElement.attr("content")
+                Timber.d("$imageUrl")
+            }
+        }
+
+        withContext(Dispatchers.Main) {
+            imageUrl?.let {
+                Glide
+                    .with(this@setOfficeUrl)
+                    .load(it)
+                    .into(this@setOfficeUrl)
+            }
+        }
+    }
 }
